44fda6c469a76874c092e2079008130c7c7005cb
LALIT UNEVAL <unevallalit6499@gmail.com>
2026-04-21 16:30:56 +0530

    Fixing Radis v3

--- src/main/java/com/lalit/relocationservice/RelocationServiceApplication.java
+++ src/main/java/com/lalit/relocationservice/RelocationServiceApplication.java
@@ -2,6 +2,7 @@ package com.lalit.relocationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -9,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableCaching
 //@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class RelocationServiceApplication {
 
